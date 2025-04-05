80225e6958a865a80f1cf5e50cab7a48e5daf1bc
axelle <kadance9720@gmail.com>
2025-04-05 18:39:52 +0200

    Create a csv file from web page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 url = "https://www.gov.uk/"
@@ -10,7 +11,21 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Extracting specific class
 resultats = soup.find(class_='gem-c-cards')
 
-# Extracting links and their titles
+titles_info = []
 links_titles = resultats.find_all('a', class_='govuk-link gem-c-cards__link gem-c-force-print-link-styles')
-for title in links_titles:
-    print(title.text)
+for titles in links_titles:
+    titles_info.append(titles.text)
+
+descriptions_info = []
+links_descriptions = resultats.find_all('p', class_='govuk-body gem-c-cards__description')
+for descriptions in links_descriptions:
+    descriptions_info.append(descriptions.text)
+
+headings = ["Titles", "Descriptions"]
+
+with open('gov_datas.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(headings)
+    for titles, descriptions in zip(titles_info, descriptions_info):
+        line = [titles, descriptions]
+        writer.writerow(line)
