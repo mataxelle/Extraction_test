70d09d0e7850d0959438c38e2b63ab2e80d84e4b
axelle <kadance9720@gmail.com>
2025-04-06 19:24:05 +0200

    Refactoring

--- main.py
+++ main.py
@@ -2,30 +2,29 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://www.gov.uk/"
-page = requests.get(url)
+def extracting():
+    url = "https://www.gov.uk/"
+    page = requests.get(url)
+    # Extracting the desired information with Beautiful Soup
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # Extracting specific class
+    resultats = soup.find(class_='gem-c-cards')
+    return resultats
 
-# Extracting the desired information with Beautiful Soup
-soup = BeautifulSoup(page.content, 'html.parser')
+def transforming(container, tag, class_name):
+    # Extracts all texts matching a specific tag and class in a given container
+    return [element.text for element in container.find_all(tag, class_=class_name)]
 
-# Extracting specific class
-resultats = soup.find(class_='gem-c-cards')
-
-titles_info = []
-links_titles = resultats.find_all('a', class_='govuk-link gem-c-cards__link gem-c-force-print-link-styles')
-for titles in links_titles:
-    titles_info.append(titles.text)
-
-descriptions_info = []
-links_descriptions = resultats.find_all('p', class_='govuk-body gem-c-cards__description')
-for descriptions in links_descriptions:
-    descriptions_info.append(descriptions.text)
+titles_info = transforming(extracting(), 'a', 'govuk-link gem-c-cards__link gem-c-force-print-link-styles')
+descriptions_info = transforming(extracting(), 'p', 'govuk-body gem-c-cards__description')
 
 headings = ["Titles", "Descriptions"]
 
-with open('gov_datas.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(headings)
-    for titles, descriptions in zip(titles_info, descriptions_info):
-        line = [titles, descriptions]
-        writer.writerow(line)
+def loading():
+    with open('gov_datas.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(headings)
+        writer.writerows(zip(titles_info, descriptions_info))
+
+if __name__ == "__main__":        
+    loading()
